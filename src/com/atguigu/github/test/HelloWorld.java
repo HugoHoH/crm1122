fdc31ee1376403dbfdf467d4a3224d7078174440
HugoHoH <1228564625@qq.com>
2016-05-02 20:22:11 +0800

    response01

--- src/com/atguigu/github/test/HelloWorld.java
+++ src/com/atguigu/github/test/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello My first github");
 		System.out.println("github, 第二次request");
+		System.out.println("收到request,完毕");
 	}
 }
